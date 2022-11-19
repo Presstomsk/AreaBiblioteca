917bd07d40f1d1864273e3b387b55a250f1098e1
Roman Permiakov <info@presstomsk.ru>
2022-11-19 15:28:07 +0700

    Add files via upload

---
+++ SqlZadanie/SQLTest.sql
@@ -0,0 +1,23 @@
+Create table Products(
+Id int Primary key not null, 
+ProductName nvarchar(500) not null); /*Таблица продуктов*/
+
+Create table Categories( 
+Id int Primary key not null, 
+CategoryName nvarchar(500) not null); /*Таблица категорий*/
+
+Create table ProductsAndCategories(
+Id int Primary key not null,
+ProductsNameId int Foreign key references Products(Id),
+CategoryNameId int Foreign key references Categories(Id)); /*Таблица связи продуктов и категорий*/
+
+insert into Products values (1,'Мобилка_1'), (2,'Машинка'), (3,'Домик'), (4,'Верблюд'), (5,'Мобилка_2');
+
+insert into Categories values (1,'Телефон'), (2,'Средства связи'), (3,'Автомобили'), (4,'Дома');
+
+insert into ProductsAndCategories values (1,1,1), (2,1,2), (3,2,3), (4,3,4), (5,5,1), (6,5,2);
+
+select ProductName, CategoryName 
+from Products 
+left join ProductsAndCategories on Products.Id = ProductsAndCategories.ProductsNameId 
+left join Categories on ProductsAndCategories.CategoryNameId = Categories.Id; /*Наш JOIN*/ 
